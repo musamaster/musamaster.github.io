cb41e7268bc49684cf91dca8d18fb3aba2246155
musamaster <43011459+musamaster@users.noreply.github.com>
2019-12-08 10:10:34 -0800

    color change

--- CoskuWebsite/js/app.js
+++ CoskuWebsite/js/app.js
@@ -71,7 +71,7 @@ function init() {
   var lights = [];
 lights[0] = new THREE.DirectionalLight( 0xffffff, 1 );
 lights[0].position.set( 1, 0, 0 );
-lights[1] = new THREE.DirectionalLight( 0x11E8BB, 1 );
+lights[1] = new THREE.DirectionalLight( 0xf2f1bf, 1 );
 lights[1].position.set( 0.75, 1, 0.5 );
 lights[2] = new THREE.DirectionalLight( 0x1B002A, 1 );
 lights[2].position.set( -0.75, -1, 0.5 );
